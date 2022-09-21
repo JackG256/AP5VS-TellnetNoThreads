4bfc7b65c2766ea0ee90042f926c896c27de74bc
l_kollar <ancovicka123>
2022-09-21 10:46:04 +0200

    Implemented basic functionality

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,28 @@
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.Socket;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        try {
+            Socket s = new Socket("smtp.utb.cz", 25);
+            InputStream inp = s.getInputStream();
+            OutputStream outp = s.getOutputStream();
+            int znak;
+            while (true) {
+                if (inp.available() > 0) {
+                    znak = inp.read();
+                    System.out.print((char) znak);
+                }
+                if (System.in.available() > 0) {
+                    outp.write(System.in.read());
+                    outp.flush();
+                }
+                Thread.sleep(10);
+            }
+        } catch (IOException | InterruptedException e) {
+            e.printStackTrace();
+        }
     }
 }
